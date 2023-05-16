a7048bc0777a061774d8619baa1c6c1e270fa267
Rajan17104 <guddurani.294@gmail.com>
2023-05-16 10:23:41 +0530

    16-5-movie

---
+++ assest/js/index.js
@@ -0,0 +1,25 @@
+let submitRef = document.getElementById("login-form");
+
+
+const handlesubmit = () =>{
+
+    let email= document.getElementById("email").value;
+    let pasw = document.getElementById("password").value;
+
+    console.log(email , pasw);
+
+    if(email=== 'abc@gmail.com' && pasw=== '123'){
+        alert("Admin page login successfully");
+        window.location = "../admin/adhome-page.html";
+    }else if(email !== 'abc@gmail.com'){
+        alert("Invaild E-mail");
+    }else if(pasw !== '125'){
+        alert("Invaild Password")
+    }
+
+    event.preventDefault();
+
+
+}
+
+submitRef.addEventListener("submit",handlesubmit);
