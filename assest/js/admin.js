8359ca9e065a79c359b50dd5db17270e0d846671
Rajan17104 <rajanhingarajiya17104@gmail.com>
2023-05-17 12:26:06 +0530

    17-5-movie

--- assest/js/admin.js
+++ assest/js/admin.js
@@ -7,14 +7,14 @@ const handleCinema = () =>{
 
     document.getElementById("data").style.display="block";
     
-    let val = document.getElementById("cinemaForm").value;
     
-    console.log(val);
 
 
     event.preventDefault();
 
 };
+
+
 
 
 // cinemaRef.addEventListener("onclick",handleCinema);
