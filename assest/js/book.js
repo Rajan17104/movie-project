9e67b1e6acb2a19888def14936c4c9c03166b046
Rajan17104 <rajanhingarajiya17104@gmail.com>
2023-06-02 06:46:52 +0530

    1-6-movie

--- assest/js/book.js
+++ assest/js/book.js
@@ -1,76 +1,125 @@
-const bookonload = () => {
+// const bookonload = () => {
+
+//     let localmovie = JSON.parse(localStorage.getItem("movie"));
+//     let localdata = JSON.parse(localStorage.getItem("cinema"));
+
+//     let movieName = JSON.parse(sessionStorage.getItem("moviename"));
+
+//     let Mdata = localmovie.filter((v) => v.name === movieName);
+
+//     // console.log(Mdata);
+
+//     Mdata.map((v) => {
+
+//         let marr = [];
+
+//         marr.push(v.cid);
+//         console.log(marr);
+        
+//         let mprint = '<div class = "cinemaData">'
+
+//         mprint += '<div class="Cname">' + 'Movie name :' + v.name + '</div>' + '<br>'
+           
+//         mprint += '</div>'
+//         document.getElementById("movie").innerHTML = mprint;
+
+
+//         let carr = [];
+
+//         carr.push(localdata);
+//         // console.log(carr);
+
+//         carr.map((a) => {
+//             a.map((b) => {
+//                 let carr1 = [];
+//                 carr1.push(b.cid);
+//                 // console.log(b.cid);
+
+//                 for(let i=0; i<marr.length; i++){
+
+//                 if (b.cid === parseInt(v.cid)) {
+//                     console.log(parseInt(v.cid));
+
+//                     let localdata = JSON.parse(localStorage.getItem("cinema"));
+
+//                     let Cdata = localdata.filter((cv) => cv.cid === parseInt(v.cid));
+
+//                     console.log(Cdata);
+                    
+//                     let cprint = '<div class = "cinemaData">'
+
+//                         Cdata.map((v) => {
+//                             // cprint += '<div class="data">'
+//                             // cprint += '<a href="../user/cinemaDetails.html" class ="data" onclick="handleCinemaDetails(' + v.cid + ')">'
+//                             cprint += '<div class="Cname">' + 'name :' + v.name + '</div>' + '<br>'
+//                             cprint += '<div class="Cname">' + 'location :' + v.location + '</div>' + '<br>'
+//                             cprint += '<div class="Cname">' + 'facilites :' + v.facilites + '</div>'
+//                             // cprint += '</div>'
+//                             // cprint += '</a>'
+
+//                         });
+
+//                     cprint += '</div>'
+//                     // document.getElementById("cinema").innerHTML = Cdara
+//                     document.getElementById("cinema").innerHTML = cprint;
+
+//                 };       
+//                 }
+//             });
+//         });
+
+//     });
+
+//     // document.getElementById("bookDisplay").innerHTML= marr[0].Mdata;
+//     event.preventDefault();
+// }
+
+
+const bookonload =() =>{
 
     let localmovie = JSON.parse(localStorage.getItem("movie"));
-    let localdata = JSON.parse(localStorage.getItem("cinema"));
 
     let movieName = JSON.parse(sessionStorage.getItem("moviename"));
 
     let Mdata = localmovie.filter((v) => v.name === movieName);
 
-    // console.log(Mdata);
+    let arr =[];
 
-    Mdata.map((v) => {
+    Mdata.map((v) =>{
+        arr.push(v.cid);
+        console.log(arr);
 
-        let marr = [];
-
-        marr.push(v.cid);
-        // console.log(marr);
-        
         let mprint = '<div class = "cinemaData">'
 
-        mprint += '<div class="Cname">' + 'Movie name :' + v.name + '</div>' + '<br>'
-           
+            mprint += '<div class="Cname">' + 'Movie name :' + v.name + '</div>' + '<br>'
+                   
         mprint += '</div>'
-        document.getElementById("movie").innerHTML = mprint;
+        document.getElementById("movie").innerHTML = mprint;        
 
+        let localdata = JSON.parse(localStorage.getItem("cinema"));
 
-        let carr = [];
+        let cprint = '<div class = "cinemaData">'
 
-        carr.push(localdata);
-        // console.log(carr);
+        localdata.map((v) =>{
+            for(let i=0; i<arr.length; i++){
 
-        carr.map((a) => {
-            a.map((b) => {
-                let carr1 = [];
-                carr1.push(b.cid);
-                // console.log(b.cid);
+                if(v.cid === parseInt(arr[i])){
+                    cprint += '<div class="Cname">' + 'name :' + v.name + '</div>' + '<br>'
+                    cprint += '<div class="Cname">' + 'location :' + v.location + '</div>' + '<br>'
+                    cprint += '<div class="Cname">' + 'facilites :' + v.facilites + '</div>'+'<br>'
+                    cprint += '<div class="Cname">' + '</div>'+'<br>'
+                }
 
-                if (b.cid === parseInt(v.cid)) {
-                    console.log(parseInt(v.cid));
+            }   
+        })
 
-                    let localdata = JSON.parse(localStorage.getItem("cinema"));
+        cprint += '</div>'
 
-                    let Cdata = localdata.filter((cv) => cv.cid === parseInt(v.cid));
-
-                    console.log(Cdata);
-                    
-                    let cprint = '<div class = "cinemaData">'
-
-                        Cdata.map((v) => {
-                            // cprint += '<div class="data">'
-                            // cprint += '<a href="../user/cinemaDetails.html" class ="data" onclick="handleCinemaDetails(' + v.cid + ')">'
-                            cprint += '<div class="Cname">' + 'name :' + v.name + '</div>' + '<br>'
-                            cprint += '<div class="Cname">' + 'location :' + v.location + '</div>' + '<br>'
-                            cprint += '<div class="Cname">' + 'facilites :' + v.facilites + '</div>'
-                            // cprint += '</div>'
-                            // cprint += '</a>'
-
-                        });
-
-                    cprint += '</div>'
-                    // document.getElementById("cinema").innerHTML = Cdara
-                    document.getElementById("cinema").innerHTML = cprint;
-
-                };                      
-            });
-        });
+        document.getElementById("cinema").innerHTML=cprint
 
     });
 
-    // document.getElementById("bookDisplay").innerHTML= marr[0].Mdata;
-    event.preventDefault();
 }
-
 
 
 
