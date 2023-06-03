da6a28f2a9639dd8e510e9b3b6e0fa5084dc4dca
Rajan17104 <rajanhingarajiya17104@gmail.com>
2023-06-03 11:54:52 +0530

    3-6-movie

--- assest/js/seatBook.js
+++ assest/js/seatBook.js
@@ -5,30 +5,48 @@ const seatonload = () => {
     let seatdata = JSON.parse(sessionStorage.getItem("seatdata"));
     let localseat = JSON.parse(localStorage.getItem("seat"));
 
-    console.log(moviename);
-    console.log(cinemaid);
-    console.log(seatdata);
-    console.log(localseat);
+    // console.log(moviename);
+    // console.log(cinemaid);
+    // console.log(seatdata);
+    // console.log(localseat);
     
-    let Sdata = localseat.filter((v) => v.cid === cinemaid);
+    let Sdata = localseat.filter((v) => v.cid === cinemaid && v.time == seatdata);
 
-    console.log(Sdata);
+    // console.log(Sdata);
 
-    Sdata.map((v,i) =>{
-
-        for(let i=0; i < v.seat.length; i++){
-            sprint = '<div class = "data">'
-                // sprint += `<div class="data">Seat :<button/>${v.seat}</button></div>`;
-                sprint += '<div class="data">Seat :<button/>'+v.seat+'</button></div>';
-            sprint += '</div>'
-        };
-
-        document.getElementById("seat").innerHTML = sprint;
-
-    });
    
+    let sprint = '<div class="cinemaData">'
+        Sdata.map((v)=>{
+     
+            for (let i = 0; i < v.seat.length; i++) {
+                sprint += '<button class="btn btn-primary" id="seatbtn" onclick="Bookseat(' + v.sid + ')">' + v.seat[i] + '</button>';
+            };
+        
+        })
+     
+    sprint += '</div>'
+    document.getElementById("seat").innerHTML = sprint;
 
-};
+  
+}
+
+
+const Bookseat = (sid,i) =>{
+
+    // document.getElementById('seat'+ i).innerHTML = 1; 
+
+    let localseat = JSON.parse(localStorage.getItem("seat"));
+
+    seatData = localseat.filter((v) => sid === v.sid);
+
+    console.log(seatData);
+
+    seatData.map((v) => v.seat[i]= 1)    
+
+}
+
+
+
 
 
 window.onload =seatonload;
